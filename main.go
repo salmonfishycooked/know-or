6bf654b9657d102f52de169fc152564650070639
salmonfishycooked <salmonfishycooked@gmail.com>
2023-04-28 09:39:06 +0800

    添加 初始化分布式 ID 生成器

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"go_web_app/dao/mysql"
 	"go_web_app/dao/redis"
 	"go_web_app/logger"
+	"go_web_app/pkg/snowflake"
 	"go_web_app/routes"
 	"go_web_app/settings"
 	"log"
@@ -19,36 +20,37 @@ import (
 )
 
 func main() {
-	// 1. 加载配置
+	// 加载配置
 	if err := settings.Init(); err != nil {
 		fmt.Printf("init settings failed, err:%v\n", err)
 		return
 	}
-
-	// 2. 初始化日志
+	// 初始化日志
 	if err := logger.Init(settings.Conf.LogConfig); err != nil {
 		fmt.Printf("init logger failed, err:%v\n", err)
 		return
 	}
-
-	// 3. 初始化 MySQL 连接
+	// 初始化 MySQL 连接
 	if err := mysql.Init(settings.Conf.MySQLConfig); err != nil {
 		fmt.Printf("init mysql failed, err:%v\n", err)
 		return
 	}
 	defer mysql.Close()
-
-	// 4. 初始化 Redis 连接
+	// 初始化 Redis 连接
 	if err := redis.Init(settings.Conf.RedisConfig); err != nil {
 		fmt.Printf("init redis failed, err:%v\n", err)
 		return
 	}
 	defer redis.Close()
-
-	// 5. 注册路由
+	// 初始化分布式 ID 生成器
+	if err := snowflake.Init(settings.Conf.StartTime, settings.Conf.MachineID); err != nil {
+		fmt.Printf("init snowflake failed, err:%v\n", err)
+		return
+	}
+	// 注册路由
 	r := routes.Setup()
 
-	// 6. 启动服务（优雅关机）
+	// 启动服务（优雅关机）
 	srv := &http.Server{
 		Addr:    fmt.Sprintf(":%d", viper.GetInt("app.port")),
 		Handler: r,
