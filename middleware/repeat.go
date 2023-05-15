95dca2ffeec5b525b33d0054b542d3d44bdb4ae8
salmonfishycooked <salmonfishycooked@gmail.com>
2023-05-15 17:15:32 +0800

    fix-优化代码

--- middleware/repeat.go
+++ middleware/repeat.go
@@ -8,35 +8,37 @@ import (
 )
 
 // IsRepeatLogin 限制同一账号只能在一台设备上登录
-func IsRepeatLogin(c *gin.Context) {
-	// 拿取该请求的用户uid
-	uid, err := controller.GetCurrentUser(c)
-	if err != nil {
-		e.ResponseError(c, e.CodeNeedLogin)
-		c.Abort()
-		return
-	}
+func IsRepeatLogin() func(*gin.Context) {
+	return func(c *gin.Context) {
+		// 拿取该请求的用户uid
+		uid, err := controller.GetCurrentUser(c)
+		if err != nil {
+			e.ResponseError(c, e.CodeNeedLogin)
+			c.Abort()
+			return
+		}
 
-	// 去缓存里面取对应的用户 token
-	rToken, err := redis.GetUserToken(uid)
-	if err != nil {
-		e.ResponseError(c, e.CodeNeedLogin)
-		c.Abort()
-		return
-	}
-	// 取当前请求 token
-	token, err := controller.GetCurrentUserToken(c)
-	if err != nil {
-		e.ResponseError(c, e.CodeNeedLogin)
-		c.Abort()
-		return
-	}
+		// 去缓存里面取对应的用户 token
+		rToken, err := redis.GetUserToken(uid)
+		if err != nil {
+			e.ResponseError(c, e.CodeNeedLogin)
+			c.Abort()
+			return
+		}
+		// 取当前请求 token
+		token, err := controller.GetCurrentUserToken(c)
+		if err != nil {
+			e.ResponseError(c, e.CodeNeedLogin)
+			c.Abort()
+			return
+		}
 
-	// 对比 token，一致则通过，不一致则返回错误
-	if rToken != token {
-		e.ResponseError(c, e.CodeLoginRepeat)
-		c.Abort()
-		return
+		// 对比 token，一致则通过，不一致则返回错误
+		if rToken != token {
+			e.ResponseError(c, e.CodeLoginRepeat)
+			c.Abort()
+			return
+		}
+		c.Next()
 	}
-	c.Next()
 }
