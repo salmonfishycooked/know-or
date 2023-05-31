2dd67aa7b0ce9974e4ec5ca43d612ddca562e1d9
salmonfishycooked <salmonfishycooked@gmail.com>
2023-05-31 14:07:27 +0800

    修改细节

--- dao/redis/keys.go
+++ dao/redis/keys.go
@@ -3,7 +3,7 @@ package redis
 // redis key 使用命名空间的方式，方便查询和拆分
 
 const (
-	keyPrefix = "bluebell:"
+	keyPrefix = "know-or:"
 
 	KeyPostTimeZSet  = "post:time"  // zset; 帖子及发帖时间
 	KeyPostScoreZSet = "post:score" // zset; 帖子及投票的分数
