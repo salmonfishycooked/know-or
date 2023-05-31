efc99145dabda9f10d78c6628f76aca16afebd02
salmonfishycooked <salmonfishycooked@gmail.com>
2023-05-31 14:41:00 +0800

    调整路由

--- routes/routes.go
+++ routes/routes.go
@@ -20,25 +20,34 @@ func Setup() *gin.Engine {
 	r.Use(logger.GinLogger(), logger.GinRecovery(true))
 
 	v1 := r.Group("/api/v1")
-	v1.Use(middleware.RateLimitMiddleware(BUCKET_RATE, BUCKET_CAP)) // 令牌桶限流中间件
+
+	// 令牌桶限流中间件
+	v1.Use(middleware.RateLimitMiddleware(BUCKET_RATE, BUCKET_CAP))
+
 	// 用户业务路由
 	v1.POST("/signup", controller.SignUpHandler)
 	v1.POST("/login", controller.LoginHandler)
 
-	v1.Use(middleware.JWTAuthMiddleware())       // 应用 JWT 认证中间件
-	v1.Use(middleware.IsRepeatLoginMiddleware()) // 单点登录检查
+	post := v1.Group("/post")
 	{
-		v1.GET("/community", controller.CommunityHandler)
-		v1.GET("/community/:id", controller.CommunityDetailHandler)
-
-		v1.POST("/post", controller.CreatePostHandler)
-		v1.GET("/post/:id", controller.GetPostDetailHandler)
-		//v1.GET("/posts", controller.GetPostListHandler)
+		post.GET("/:id", controller.GetPostDetailHandler)
 		// 根据时间或分数获取帖子列表
-		v1.GET("/posts", controller.GetPostListHandler)
+		post.GET("/", controller.GetPostListHandler)
+
+		// 对以下接口应用中间件
+		post.Use(middleware.JWTAuthMiddleware())
+		post.Use(middleware.IsRepeatLoginMiddleware())
 
 		// 投票
-		v1.POST("/vote", controller.PostVoteHandler)
+		post.POST("/vote", controller.PostVoteHandler)
+		// 新建帖子
+		post.POST("/new", controller.CreatePostHandler)
+	}
+
+	community := v1.Group("/community")
+	{
+		community.GET("/", controller.CommunityHandler)
+		community.GET("/:id", controller.CommunityDetailHandler)
 	}
 
 	return r
